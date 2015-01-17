34d14472fcdcc4e652797184102cef6f26145cec
Yoshi Fujimoto <yosh.fuj@gmail.com>
2015-01-17 02:42:42 -0500

    Google Places API integrated

--- Google Places Test/ViewController.swift
+++ Google Places Test/ViewController.swift
@@ -12,6 +12,7 @@ import CoreLocation
 class ViewController: UIViewController, CLLocationManagerDelegate {
 
     let locationManager = CLLocationManager()
+    var location: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 47.620506, longitude: -122.349277)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,19 +20,26 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         
         // Remember to include corelocation and mapkit frameworks
         
-        
+        // Collect current user location
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
         
+        var gp = GooglePlaces()
+        gp.search(location, radius: 2000, query: "coffee") { (items, errorDescription) -> Void in
+            
+            println("Result count: \(items!.count)")
+            for index in 0..<items!.count {
+                println([items![index].name])
+            }
+        }
         
     }
 
     func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
         var userLocation: CLLocation = locations[0] as CLLocation
-        println(userLocation.coordinate.latitude)
-        println(userLocation.coordinate.longitude)
+        location = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
     }
     
     func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
